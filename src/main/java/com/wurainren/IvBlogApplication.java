f61306afb57d78f8a1806f11067996a0197a6417
renwu <l>
2017-12-12 19:02:56 +0800

    修改登录界面及前端bootstrap

--- src/main/java/com/wurainren/IvBlogApplication.java
+++ src/main/java/com/wurainren/IvBlogApplication.java
@@ -1,12 +1,21 @@
 package com.wurainren;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IvBlogApplication {
+public class IvBlogApplication   extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IvBlogApplication.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class applicationClass = Application.class;
 }
